46619cf5f792bf08233e01be3e4a0bfcaefbb43a
Yeetmeister <k.t.noot@live.nl>
2024-09-02 17:54:24 +0200

    Add first part of character view page

--- lib/pages/charview.dart
+++ lib/pages/charview.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:dandy/json_models/character.dart';
 import 'package:dandy/page.dart';
+import 'package:dandy/services/property_getter.dart';
 import 'package:dandy/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -14,9 +17,30 @@ class CharacterViewPage extends BasePage {
 
 class _CharViewPageState extends State<CharacterViewPage> {
   late final Character _character = widget.character;
+  late final PropertyGetter _prop = PropertyGetter(_character);
+  late final Image? _image = _character.imageBase64 != ""
+    ? Image.memory(base64Decode(_character.imageBase64))
+    : null;
 
   @override
   Widget build(BuildContext context) {
-    return basicScaffold(context, widget);
+    return basicScaffold(context, widget, body: Padding(
+      padding: const EdgeInsets.all(10),
+      child: ListView(
+        children: [
+          Column(
+            children: [
+              IconButton.outlined(
+                onPressed: () {},
+                icon: _image ?? const Icon(Icons.person),
+                iconSize: 48,
+                style: const ButtonStyle(shape: WidgetStatePropertyAll(ContinuousRectangleBorder())),
+                padding: _image == null ? null : const EdgeInsets.all(4),
+              ),
+            ],
+          ),
+        ]
+      ),
+    ));
   }
 }
